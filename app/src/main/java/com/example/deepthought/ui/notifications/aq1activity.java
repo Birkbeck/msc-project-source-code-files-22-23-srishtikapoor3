36de1996154b0f2b187c67260b9a41450411722d
srishtikapoor <srishtikapoor3@gmail.com>
2023-09-21 22:12:20 +0100

    intial screens and layout fot the app.

---
+++ app/src/main/java/com/example/deepthought/ui/notifications/aq1activity.java
@@ -0,0 +1,16 @@
+package com.example.deepthought.ui.notifications;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.deepthought.R;
+
+public class aq1activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aq1activity);
+    }
+}
